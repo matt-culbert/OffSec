30761c06f97474fdb1c232fb156084d348a1dd05
matt-culbert <culbertmatt@gmail.com>
2021-11-20 11:02:20 -0500

    Update server.py

--- Switchblade/server.py
+++ Switchblade/server.py
@@ -6,14 +6,14 @@ import ssl
 
 listen_addr = '192.168.202.135'
 listen_port = 8082
-server_cert = '/etc/nginx/certs/server.crt'
-server_key = '/etc/nginx/certs/server.key'
-client_certs = '/etc/nginx/certs/ca.crt'
+#server_cert = '/etc/nginx/certs/server.crt'
+#server_key = '/etc/nginx/certs/server.key'
+#client_certs = '/etc/nginx/certs/ca.crt'
 
-context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-context.verify_mode = ssl.CERT_REQUIRED
-context.load_cert_chain(certfile=server_cert, keyfile=server_key)
-context.load_verify_locations(cafile=client_certs)
+#context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+#context.verify_mode = ssl.CERT_REQUIRED
+#context.load_cert_chain(certfile=server_cert, keyfile=server_key)
+#context.load_verify_locations(cafile=client_certs)
 
 bindsocket = socket.socket()
 bindsocket.bind((listen_addr, listen_port))
